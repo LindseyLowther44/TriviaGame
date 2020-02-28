5ce84daf4ae43d5104901404a6594f822a8844e8
LindseyLowther44 <lindseymlowther@gmail.com>
2020-02-27 18:29:50 -0600

    Added Css Styling and Javascript Questions

---
+++ assets/javascript/game.js
@@ -0,0 +1,101 @@
+const myQuestions = [
+        {     
+            question: "What does Michael Scott’s mug say?",
+            answers: {
+                a: "Toby is the worst!" ,
+                b: "Worlds Best Boss",
+                c: "The Office",
+                d: "I love Toby!"
+        },
+            correctAnswer: "b"
+        },
+        {
+            question: "What is the name of Jim and Pams child?",
+            answers: {
+                a: "Michael",
+                b: "Astrid",
+                c: "Phoebe",
+                d: "Cici"
+        },
+            correctAnswer: "d"
+        },
+        {
+            question: "Name Ryan’s online social networking site?",
+            answers: {
+                a: "He never made one",
+                b: "MrEntreprenuer.com",
+                c: "WUPHF.com",
+                d: "Isuck.com"
+        },
+            correctAnswer: "c"
+        },
+        {
+            question: "Who attended anger management in season 3?",
+            answers: {
+                a: "Dwight",
+                b: "Andy",
+                c: "Kevin",
+                d: "Kelly"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What is Toby the Thief of?",
+            answers: {
+                a: "Happiness",
+                b: "Joy",
+                c: "Life",
+                d: "Everything"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "Where did Jim propose to Pam?",
+            answers: {
+                a: "The Office",
+                b: "Poor Richards",
+                c: "Outside of a Gas Station",
+                d: "In front of their home."
+            },
+            correctAnswer: "c" 
+        },
+        {
+            question: "Where did Holly & Michael move to?",
+                answers: {
+                    a: "Colorado",
+                    b: "California",
+                    c: "Neither, They stayed in Scranton.",
+                    d: "Vegas"
+                },
+                correctAnswer: "a"
+        },
+        {
+            question: "Who does Jim impersonate one day at the office for 11 dollars?",
+            answers: {
+                a: "Ryan",
+                b: "Michael",
+                c: "Stanley",
+                d: "Dwight"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "What is Micheal proud of in his new condo?",
+            answers: {
+                a: "Jan",
+                b: "A very small Plasma screen TV",
+                c: "The Windows",
+                d: "The Webcam"
+
+            },
+            correctAnswer: "b"
+        }    
+];
+
+
+var score = 0;
+
+
+function showQuestions ()
+
+
---
+++ assets/javascript/questions.js
@@ -0,0 +1,9 @@
+What does Michael Scott’s mug say? Worlds best boss
+Name Jim’s and Pam’s baby. CICI
+Name Ryan’s online social networking site. WUPHF.com
+Who attended anger management in season 3? Andy
+What is the name of the character in Michaels movie? Michael Scarn
+Where did Jim Propose to Pam? Outside a gas station.
+Where did Michael move to with Holly? Colorado
+What was Michael really proud of buying for his Condo? A very small plasma screen TV
+
